1474ad90b578b8e4dfdfafcd9a9f8f3a6c729875
disias <diogenes.dauster@gmail.com>
2017-10-08 18:38:01 -0300

    Iniciando o repositorio !!

---
+++ wear/src/main/java/com/example/dauster/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.dauster.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = findViewById(R.id.text);
+    }
+}
